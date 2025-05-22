5ef44863b54c6a31e56658fee422914f3517b7f0
Magnus Marthinsen <magnus.marthinsen@bouvet.no>
2025-05-22 10:06:41 +0200

    Add db context and configuration

--- src/Cookbook.Api/Program.cs
+++ src/Cookbook.Api/Program.cs
@@ -1,8 +1,17 @@
+using Cookbook.Domain;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<CookbookContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CookbookDatabase"));
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ src/Cookbook.Domain/CookbookContext.cs
@@ -0,0 +1,40 @@
+﻿using Cookbook.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cookbook.Domain;
+
+public class CookbookContext(DbContextOptions<CookbookContext> builderOptions) : DbContext(builderOptions)
+{
+    public DbSet<Recipe> Recipes { get; set; }
+    public DbSet<Ingredient> Ingredients { get; set; }
+    public DbSet<Step> Steps { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Recipe>()
+            .Property(b => b.Name)
+            .HasMaxLength(50)
+            .IsRequired();
+        
+        modelBuilder.Entity<Ingredient>()
+            .Property(b => b.Amount)
+            .HasPrecision(7, 2);
+        
+        modelBuilder.Entity<Ingredient>()
+            .Property(b => b.Name)
+            .HasMaxLength(50)
+            .IsRequired();
+
+        modelBuilder.Entity<Unit>()
+            .ToTable("Units");
+        
+        modelBuilder.Entity<Unit>()
+            .Property(b => b.Id)
+            .ValueGeneratedNever();
+        
+        modelBuilder.Entity<Unit>()
+            .Property(b => b.Name)
+            .HasMaxLength(100)
+            .IsRequired();
+    }
+}
