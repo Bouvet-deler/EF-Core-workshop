f93f6ee7f29dd9c831e275145dc47fc754f8c12a
Magnus Marthinsen <magnus.marthinsen@bouvet.no>
2025-05-22 18:22:50 +0200

    Added endpoint for getting recipes by ingredients

--- src/Cookbook.Api/Controllers/RecipeController.cs
+++ src/Cookbook.Api/Controllers/RecipeController.cs
@@ -28,4 +28,23 @@ public class RecipeController(CookbookContext dbContext) : ControllerBase
         // Please don't return the entity directly, use a DTO instead
         return Ok(recipe);
     }
+    
+    [HttpGet]
+    [ProducesResponseType(typeof(Recipe[]), StatusCodes.Status200OK)]
+    public async Task<IActionResult> Get([FromQuery] string[] ingredientNames)
+    {
+        var recipeQuery = dbContext.Recipes
+            .Where(recipe => recipe.Ingredients
+                .Any(ingredient => 
+                    ingredientNames.Any(name => ingredient.Name == name)))
+            .Include(recipe => recipe.Ingredients)
+            .ThenInclude(ingredient => ingredient.Unit)
+            .Include(recipe => recipe.Steps)
+            .AsSplitQuery();
+            
+        var recipes = await recipeQuery // Break here and run recipeQuery.ToQueryString() in the debugger console.
+            .ToListAsync(HttpContext.RequestAborted);
+    
+        return Ok(recipes);
+    }
 }
