debd3d264e67358ecd4e3a7d343012f922a54354
Magnus Marthinsen <magnus.marthinsen@bouvet.no>
2025-05-22 18:03:18 +0200

    Added endpoint for getting a recipe

---
+++ src/Cookbook.Api/Controllers/RecipeController.cs
@@ -0,0 +1,31 @@
+﻿using Cookbook.Domain;
+using Cookbook.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cookbook.Api.Controllers;
+
+[ApiController]
+[Route("api/recipies")]
+public class RecipeController(CookbookContext dbContext) : ControllerBase
+{
+    [HttpGet("{recipeId:int}")]
+    [ProducesResponseType(typeof(Recipe), StatusCodes.Status200OK)]
+    public async Task<IActionResult> Get(int recipeId)
+    {
+        var recipeQuery = dbContext.Recipes
+            .Include(recipe => recipe.Ingredients)
+            .ThenInclude(ingredient => ingredient.Unit)
+            .Include(recipe => recipe.Steps)
+            .AsSplitQuery();
+            
+        var recipe = await recipeQuery // Break here and run recipeQuery.ToQueryString() in the debugger console.
+            .FirstOrDefaultAsync(recipe => recipe.Id == recipeId, HttpContext.RequestAborted);
+        
+        if (recipe == null)
+            return Problem(statusCode: StatusCodes.Status404NotFound);
+        
+        // Please don't return the entity directly, use a DTO instead
+        return Ok(recipe);
+    }
+}
